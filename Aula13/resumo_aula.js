eb30896b832338217abaa1dd07cc46e1b69cff7f
Danilo <danilocarneirop@gmail.com>
2025-05-22 14:40:49 -0300

    Aula13 - comentarios sobre TAD

--- Aula13/resumo_aula.js
+++ Aula13/resumo_aula.js
@@ -9,3 +9,9 @@ function fatorial(n) {
 }
 
 console.log(fatorial(5))
+
+// TAD - Tipos Abstratos de Dados
+// Abstração = forma de exibir os dados ao usuario
+// Encapsulamento = empacotar dados em uma estrutura
+// Ocultação de informação = interage diretamente com a interface
+// Separação entre interface e implementação = como voce vai implementar os dados em uma interface
