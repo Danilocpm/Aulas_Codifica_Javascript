bd314ceb4bc4a31e7c28af4161e85da8f4f30b8b
Danilocpm <danilocarneirop@gmail.com>
2025-06-18 23:21:07 -0300

    Ex3 da lista 2

--- Lista2/Ex3.js
+++ Lista2/Ex3.js
@@ -1,26 +1,17 @@
 // Dada uma string (ex.: "olá olá mundo mundo"), use if/else e for para extrair
 // todas as palavras únicas e exibi-las em um array.
 
-function StrToArray (str) { // receber a string
-    let word = '' // var word sendo uma str vazia
-    let newStr = [] // array da str
-    let sequence = 0 // sequencia de repeticoes
+function StrToArray (str) { // recebe a string
+    let strArray = str.trim().split(/\s+/) // Remove os espacos iniciais e finais + split pega palavras entre multiplos espacos, tabulacoes e quebra de linhas + coloca tudo em um novo array
+    let NewStrArray = [] // Novo array vazio
 
-    for(let i = 0; i < str.length; i++) { // for que repete ate o final do str
-        if(str.charAt(i) !== ' '){ // se nao for espaco entre palavras preenche a string da word
-            word += str.charAt(i)
-        } else {
-            newStr[sequence] = word // adiciona a palavra no id do array
-            sequence++ // aumenta a var de sequencia para proxima palavra ser a proxima do array
-            word = '' // reseta a var word para a nova palavra
+    for(let i = 0; i < strArray.length; i++) { // loop para percorrer todo array novo cheio
+        if(!NewStrArray.includes(strArray[i])) { // adiciona a palavra ao array vazio se ela ja nao existir nele
+            NewStrArray.push(strArray[i])
         }
     }
 
-    if(word) { // se ainda possuir uma palavra adiciona em uma nova posicao do array
-        newStr[newStr.length] = word 
-    }
-
-    return newStr
+    return NewStrArray
 }
 
 console.log(StrToArray("olá olá mundo mundo"))
